ef3dec64db9324ce5bb32441db6f1fd28f8a77fc
everlast1ngw1nter <at07012004@gmail.com>
2023-12-04 19:50:13 +0500

    test_dir_added

---
+++ OkulovskyBot/InputDataParser.cs
@@ -0,0 +1,26 @@
+﻿namespace OkulovskyBot;
+
+public class InputDataParser
+{
+    private static String nameWeightPattern = @"^(.)+ [Ww]-?{d+}&";
+    
+    public void Parse()
+    {
+        var request = new List<String>();
+        request.Add("1 w6 : 2 e7");
+        request.Add("2 w5 : 1 e1");
+        RequestParser(request);
+    }
+
+    private void RequestParser(List<String> request)
+    {
+        foreach (var line in request)
+        {
+            var splittedLine = line.Split(":");
+            if (splittedLine.Length != 2)
+            {
+                throw new ArgumentException("Incorrect separator count");
+            }
+        }
+    }
+}
