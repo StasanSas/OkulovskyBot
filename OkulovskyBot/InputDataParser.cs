5af35f8616c98396b1808fb054d0a2cd3e513cca
everlast1ngw1nter <at07012004@gmail.com>
2023-12-17 19:38:59 +0500

    primitive_parse_no_graphs

--- OkulovskyBot/InputDataParser.cs
+++ OkulovskyBot/InputDataParser.cs
@@ -21,8 +21,43 @@ public class InputDataParser
                 throw new ArgumentException("Incorrect separator count");
             }
             var nodeInfo = NodeInfo.Create(splittedLine[0]);
-            // var edgesInfo = 
+            var edgesInfo = EdgesInfo.Create(nodeInfo.Name, splittedLine[1]);
             var a = 1 + 1;
+        }
+    }
+
+    private record EdgesInfo(List<EdgeInfo> EdgeInfos)
+    {
+        private static readonly string EdgeInfoPattern = @"^( *\d+ *[eE]\d+ *)+$";
+        
+        public static EdgesInfo Create(String startNode,
+            string stringIncidentNodesData)
+        {
+            var edgeInfos = new List<EdgeInfo>();
+            var match = Regex.Match(stringIncidentNodesData, EdgeInfoPattern);
+            var groupsCount = match.Groups.Count;
+            for (var i = 1; i < groupsCount; i++)
+            {
+                edgeInfos.Add(EdgeInfo.Create(startNode, match.Groups[i].ToString()));
+            }
+            return new EdgesInfo(edgeInfos);
+        }
+    }
+    
+    private record EdgeInfo(string firstNodeName, string secondNodeName, int? Weight)
+    {
+        private static readonly string NameWeightPattern = @"^ *(\d+) *[eE](\d+) *$";
+        public static EdgeInfo Create(string firstNodeName, string inputString)
+        {
+            var match = Regex.Match(inputString, NameWeightPattern);
+            if (!match.Success)
+            {
+                throw new ArgumentException("Incorrect input for node name or weight");
+            }
+            var nodeId = match.Groups[1].ToString();
+            int? nodeWeight = match.Groups.Count < 3 ?
+                null : int.Parse(match.Groups[2].ToString());
+            return new EdgeInfo(firstNodeName, nodeId, nodeWeight);
         }
     }
 
@@ -34,7 +69,7 @@ public class InputDataParser
             Match match = Regex.Match(inputString, NameWeightPattern);
             if (!match.Success)
             {
-                throw new AggregateException("Incorrect input for node name or weight");
+                throw new ArgumentException("Incorrect input for node name or weight");
             }
             var nodeId = match.Groups[1].ToString();
             int? nodeWeight = match.Groups.Count < 3 ?
